037b44ba1295cfbb21266585456e9c7fb7ba88aa
paulpaulzhang <zlm315zlm@gmail.com>
2019-05-09 19:08:44 +0800

    update name

--- playML/tools.py
+++ playML/tools.py
@@ -2,8 +2,10 @@
 # -*- coding:utf-8 -*-
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
+# 求解梯度测试函数(万能公式)
 def dJ_DEBUG(J, theta, X_b, y, epsilon=0.01):
     res = np.empty(len(theta))
 
@@ -15,3 +17,20 @@ def dJ_DEBUG(J, theta, X_b, y, epsilon=0.01):
         res[i] = (J(theta_1, X_b, y) - J(theta_2, X_b, y)) / (2 * epsilon)
 
     return res
+
+
+# 绘制分类问题的决策边界
+def plot_decision_boundary(model, axis):
+    x0, x1 = np.meshgrid(
+        np.linspace(axis[0], axis[1], int((axis[1] - axis[0]) * 100)).reshape(-1, 1),
+        np.linspace(axis[2], axis[3], int((axis[3] - axis[2]) * 100)).reshape(-1, 1),
+    )
+    X_new = np.c_[x0.ravel(), x1.ravel()]
+
+    y_predict = model.predict(X_new)
+    zz = y_predict.reshape(x0.shape)
+
+    from matplotlib.colors import ListedColormap
+    custom_cmap = ListedColormap(['#EF9A9A', '#FFF59D', '#90CAF9'])
+
+    plt.contourf(x0, x1, zz, cmap=custom_cmap)
